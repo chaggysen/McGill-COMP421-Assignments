16ca61a704f2242bf4a31404fe7a7698cf0a52e9
chaggysen <sen.wang@mail.mcgill.ca>
2022-02-23 19:46:49 -0500

    more sc

---
+++ p2/midwifee.sql
@@ -0,0 +1,4 @@
+connect to cs421;
+
+insert into MIDWIFEINFO (mpracID, mname, mphone, memail, hname, haddress) values (38,'ima midwife', '11111111', 'vpaling14@mac.com', 'Lac-Saint-Louis', '10 main street');
+
